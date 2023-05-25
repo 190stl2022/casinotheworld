2cf3c24bbdd831c960a9bb89cbbb85b4e329b91c
190stl2022 <190@stl2022.info>
2023-05-25 16:35:48 +0800

    Initial commit

---
+++ js/about.2530570e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcasino_project"]=self["webpackChunkcasino_project"]||[]).push([[443],{7381:function(n,t,a){a.r(t),a.d(t,{default:function(){return l}});var c=a(3396);const e={class:"about"},r=(0,c._)("h1",null,"This is an about page",-1),s=[r];function u(n,t){return(0,c.wg)(),(0,c.iD)("div",e,s)}var o=a(89);const i={},f=(0,o.Z)(i,[["render",u]]);var l=f}}]);
+//# sourceMappingURL=about.2530570e.js.map
